0db64bd7681410ab3a24480056485edf95aec88f
Matteo Gabellini <matteo.gabellini2@studio.unibo.it>
2018-11-15 17:15:25 +0100

    ADDED error codes

--- src/main/java/core/exceptions/AbsentTokenException.java
+++ src/main/java/core/exceptions/AbsentTokenException.java
@@ -1,5 +1,9 @@
 package core.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.UNAUTHORIZED)
 public class AbsentTokenException extends Exception {
     public AbsentTokenException(String token) {
         super(String.format("Token %s is absent or unregistered. Please Register the token first.", token));
--- src/main/java/core/exceptions/AlreadyRegisteredException.java
+++ src/main/java/core/exceptions/AlreadyRegisteredException.java
@@ -1,5 +1,9 @@
 package core.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.PRECONDITION_FAILED)
 public class AlreadyRegisteredException extends Exception {
 
     public AlreadyRegisteredException(String token) {
